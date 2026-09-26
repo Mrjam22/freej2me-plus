427b8071a4b43ff6f8605a5e5338776bfe0d761c
Mrjam22 <155180672+Mrjam22@users.noreply.github.com>
2026-09-26 03:26:19 +0100

    Update Mobile.java

--- src/org/recompile/mobile/Mobile.java
+++ src/org/recompile/mobile/Mobile.java
@@ -256,6 +256,7 @@ public class Mobile
 	public static final int BLACKBERRY70_SOFT2 = 112;
 	public static final int BLACKBERRY70_FIRE = 8;
 	public static final int BLACKBERRY70_CLR = -88;
+	public static final int BLACKBERRY70_NUM5  = 8;
 	
 	//KDDI keycodes
 	public static final int KDDI_UP    = 1;
@@ -432,6 +433,7 @@ public class Mobile
 				case 7:  return BLACKBERRY70_FIRE; // Y
 				case 8:  return BLACKBERRY70_SOFT2; // Start
 				case 9:  return BLACKBERRY70_SOFT1; // Select
+				case 10:  return BLACKBERRY70_NUM5;
 				case 19: return BLACKBERRY70_CLR;
 			}
 		}
@@ -638,6 +640,7 @@ public class Mobile
 				case BLACKBERRY70_FIRE:  return Canvas.FIRE; // Y
 				case BLACKBERRY70_SOFT1: return Canvas.GAME_A;
 				case BLACKBERRY70_SOFT2: return Canvas.GAME_B;
+				case BLACKBERRY70_NUM5: return 	Canvas.KEY_NUM5;
 			}
 		}
 		if (kddi)
@@ -820,6 +823,7 @@ public class Mobile
 				case BLACKBERRY70_FIRE:  return Canvas.FIRE; // Y
 				case BLACKBERRY70_SOFT1: return Canvas.KEY_SOFT_LEFT; // Start   (gameAction is GAME_A, but we go with the special keys for CanvasAction)
 				case BLACKBERRY70_SOFT2: return Canvas.KEY_SOFT_RIGHT; // Select (gameAction is GAME_B, but we go with the special keys for CanvasAction)
+			    case BLACKBERRY70_NUM5:  return Canvas.KEY_NUM5;
 			}
 		}
 		if (kddi)
